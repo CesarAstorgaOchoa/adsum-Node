71477a8f17b94d071e43660edaefdeaecc60b081
CesarAstorgaOchoa <18170276@itculiacan.edu.mx>
2024-04-06 12:12:26 -0700

    backend CRUD

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.get('/',function(req,res){
     res.send('Ruta de inicio del servidor')
 })
 
+//
 app.post('/api/contacto',function(req,res){
     let data={
         NombreCompleto:req.body.NombreCompleto,
@@ -30,3 +31,57 @@ app.post('/api/contacto',function(req,res){
         }
     }) 
  })
+
+ //
+ app.get('/api/info',function(req, res){
+    connection.query('SELECT * FROM contacto', function(error, filas){
+        if(error){
+            throw error
+        }else{
+            res.send(filas)
+        }
+    })
+})
+
+//
+app.get('/api/info/:idContacto',function(req, res){
+    connection.query('SELECT * FROM contacto WHERE idContacto = ?',[req.params.idContacto], function(error, filas){
+        if(error){
+            throw error
+        }else{
+            res.send(filas)
+        }
+    })
+})
+
+//
+app.put('/api/contacto/:idContacto',function(req,res){
+    let idContacto = req.params.idContacto
+    let NombreCompleto = req.body.NombreCompleto
+    let NombreEmpresa = req.body.NombreEmpresa
+    let CorreoElectronico = req.body.Correo
+    let Telefono = req.body.Telefono
+    let Categoria = req.body.Categoria
+    let Mensaje = req.body.Mensaje
+    let sql="UPDATE contacto SET NombreCompleto = ?, NombreEmpresa=?, CorreoElectronico=?, Telefono=?, Categoria=?, Mensaje=? WHERE idContacto=?"
+    connection.query(sql,[NombreCompleto,NombreEmpresa,CorreoElectronico,Telefono,Categoria,Mensaje,idContacto],
+        function(error,results){
+            if(error){
+                throw error
+            }else{
+                res.send(results)
+            }
+        }) 
+})
+
+app.delete('/api/contacto/:idContacto',function(req,res){
+    let idContacto = req.params.idContacto
+    connection.query("DELETE FROM contacto WHERE idContacto = ?",[idContacto],
+    function(error,results){
+        if(error){
+            throw error
+        }else{
+            res.send(results)
+        }
+    })
+})
